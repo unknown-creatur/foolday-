89e908d1ffbb53e23a3d4245e8cdfdce2041b6d5
421385 <115143096+unknown-creatur@users.noreply.github.com>
2023-04-20 15:52:26 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,38 +1,9 @@
 input.onGesture(Gesture.TiltLeft, function () {
-    if (遊戲 == 0) {
-    	
-    } else {
-        _2p.change(LedSpriteProperty.X, -1)
-    }
+	
 })
 input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
     if (判斷 == 1) {
-        basic.showLeds(`
-            . # # # #
-            # # . # .
-            . # . # .
-            # # . # .
-            . # . # .
-            `)
-        basic.showLeds(`
-            . . # . .
-            # # # # #
-            # . # . #
-            # # # # #
-            . . # . .
-            `)
-        basic.showLeds(`
-            . # . # .
-            # # # # #
-            . # . # .
-            # # # # #
-            # . . . #
-            `)
-        game.setScore(0)
-        airplane = game.createSprite(0, 0)
-        _1p = game.createSprite(1, 4)
-        _2p = game.createSprite(3, 4)
-        遊戲 = 1
+    	
     } else {
         if (input.compassHeading() < 45 || input.compassHeading() > 315) {
             basic.showString("S")
@@ -74,9 +45,6 @@ input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
     }
 })
 input.onGesture(Gesture.ScreenDown, function () {
-    if (game.isRunning()) {
-        game.gameOver()
-    }
     basic.showString("" + input.temperature() + "C")
     basic.pause(1000)
     basic.showString("light:" + input.lightLevel())
@@ -91,225 +59,41 @@ input.onGesture(Gesture.ScreenDown, function () {
         basic.showString("E")
     }
     basic.pause(1000)
+    basic.showString("happy fools day!")
 })
 input.onButtonPressed(Button.A, function () {
-    if (遊戲 == 0) {
-    	
-    } else {
-        _1p.change(LedSpriteProperty.X, -1)
-    }
+	
 })
 input.onGesture(Gesture.Shake, function () {
-    if (遊戲 == 0) {
-    	
-    } else {
-        if (game.isRunning()) {
-            bullet1 = game.createSprite(2, 4)
-            bullet2 = game.createSprite(0, 4)
-            bullet3 = game.createSprite(4, 4)
-            bullet4 = game.createSprite(1, 4)
-            bullet5 = game.createSprite(3, 4)
-            for (let index = 0; index < 5; index++) {
-                for (let index = 0; index < 4; index++) {
-                    bullet1.change(LedSpriteProperty.X, -1)
-                    bullet2.change(LedSpriteProperty.X, -1)
-                    bullet3.change(LedSpriteProperty.X, -1)
-                    bullet4.change(LedSpriteProperty.X, -1)
-                    bullet5.change(LedSpriteProperty.X, -1)
-                    basic.pause(100)
-                }
-                bullet1.set(LedSpriteProperty.X, 2)
-                bullet1.set(LedSpriteProperty.Y, 4)
-                bullet2.set(LedSpriteProperty.X, 0)
-                bullet2.set(LedSpriteProperty.Y, 4)
-                bullet3.set(LedSpriteProperty.X, 4)
-                bullet3.set(LedSpriteProperty.Y, 4)
-                bullet4.set(LedSpriteProperty.X, 1)
-                bullet4.set(LedSpriteProperty.Y, 4)
-                bullet5.set(LedSpriteProperty.X, 3)
-                bullet5.set(LedSpriteProperty.Y, 4)
-            }
-            bullet1.delete()
-            bullet2.delete()
-            bullet3.delete()
-            bullet4.delete()
-            bullet5.delete()
-        }
-    }
+	
 })
 input.onButtonPressed(Button.AB, function () {
-    if (遊戲 == 0) {
-    	
-    } else {
-        if (game.isRunning()) {
-            bullet1 = game.createSprite(_1p.get(LedSpriteProperty.X), _1p.get(LedSpriteProperty.Y))
-            bullet2 = game.createSprite(_2p.get(LedSpriteProperty.X), _2p.get(LedSpriteProperty.Y))
-            for (let index = 0; index < 4; index++) {
-                bullet1.change(LedSpriteProperty.Y, -1)
-                bullet2.change(LedSpriteProperty.Y, -1)
-                basic.pause(100)
-            }
-            bullet1.delete()
-            bullet2.delete()
-        }
-    }
+	
 })
 input.onButtonPressed(Button.B, function () {
-    if (遊戲 == 0) {
-    	
-    } else {
-        _1p.change(LedSpriteProperty.X, 1)
-    }
+	
 })
 input.onGesture(Gesture.TiltRight, function () {
-    if (遊戲 == 0) {
-    	
-    } else {
-        _2p.change(LedSpriteProperty.X, 1)
-    }
+	
 })
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
     判斷 = 1
 })
-let missile2: game.LedSprite = null
-let missile1: game.LedSprite = null
-let bullet5: game.LedSprite = null
-let bullet4: game.LedSprite = null
-let bullet3: game.LedSprite = null
-let bullet2: game.LedSprite = null
-let bullet1: game.LedSprite = null
-let _1p: game.LedSprite = null
-let airplane: game.LedSprite = null
-let _2p: game.LedSprite = null
-let 遊戲 = 0
 let 判斷 = 0
 basic.showString("Hello!")
 判斷 = 0
-遊戲 = 0
 basic.forever(function () {
-    if (遊戲 == 0) {
-    	
-    } else {
-        if (game.isRunning()) {
-            if (airplane.isTouching(_1p)) {
-                game.gameOver()
-            }
-        }
-    }
+	
 })
 basic.forever(function () {
-    if (遊戲 == 0) {
-    	
-    } else {
-        if (game.isRunning()) {
-            basic.pause(randint(0, 300))
-            airplane.change(LedSpriteProperty.X, 1)
-            if (airplane.get(LedSpriteProperty.X) == 4) {
-                basic.pause(randint(0, 300))
-                airplane.change(LedSpriteProperty.Y, 1)
-                airplane.set(LedSpriteProperty.X, 0)
-            }
-        }
-    }
+	
 })
 basic.forever(function () {
-    if (遊戲 == 0) {
-    	
-    } else {
-        if (game.isRunning()) {
-            if (bullet1) {
-                if (bullet1.isTouching(airplane)) {
-                    game.addScore(1e+50)
-                    airplane.set(LedSpriteProperty.X, 0)
-                    airplane.set(LedSpriteProperty.Y, 0)
-                }
-            }
-            if (bullet2) {
-                if (bullet2.isTouching(airplane)) {
-                    game.addScore(1e+50)
-                    airplane.set(LedSpriteProperty.X, 0)
-                    airplane.set(LedSpriteProperty.Y, 0)
-                }
-            }
-            if (bullet3) {
-                if (bullet3.isTouching(airplane)) {
-                    game.addScore(1e+50)
-                    airplane.set(LedSpriteProperty.X, 0)
-                    airplane.set(LedSpriteProperty.Y, 0)
-                }
-            }
-            if (bullet4) {
-                if (bullet4.isTouching(airplane)) {
-                    game.addScore(1e+50)
-                    airplane.set(LedSpriteProperty.X, 0)
-                    airplane.set(LedSpriteProperty.Y, 0)
-                }
-            }
-            if (bullet5) {
-                if (bullet5.isTouching(airplane)) {
-                    game.addScore(1e+50)
-                    airplane.set(LedSpriteProperty.X, 0)
-                    airplane.set(LedSpriteProperty.Y, 0)
-                }
-            }
-        }
-        if (missile1) {
-            if (missile1.isTouching(airplane)) {
-                game.addScore(1e+50)
-                airplane.set(LedSpriteProperty.X, 0)
-                airplane.set(LedSpriteProperty.Y, 0)
-            }
-        }
-        if (missile2) {
-            if (missile2.isTouching(airplane)) {
-                game.addScore(1e+50)
-                airplane.set(LedSpriteProperty.X, 0)
-                airplane.set(LedSpriteProperty.Y, 0)
-            }
-        }
-    }
+	
 })
 basic.forever(function () {
-    if (遊戲 == 0) {
-    	
-    } else {
-        if (game.isRunning()) {
-            basic.pause(randint(5000, 10000))
-            missile1 = game.createSprite(2, 4)
-            missile1.turn(Direction.Right, 45)
-            basic.pause(100)
-            missile1.move(-1)
-            basic.pause(100)
-            missile1.move(-1)
-            basic.pause(100)
-            missile1.turn(Direction.Left, 90)
-            missile1.move(1)
-            basic.pause(100)
-            missile1.move(1)
-            basic.pause(100)
-            missile1.delete()
-        }
-    }
+	
 })
 basic.forever(function () {
-    if (遊戲 == 0) {
-    	
-    } else {
-        if (game.isRunning()) {
-            basic.pause(randint(5000, 10000))
-            missile2 = game.createSprite(2, 4)
-            missile2.turn(Direction.Left, 45)
-            basic.pause(100)
-            missile2.move(1)
-            basic.pause(100)
-            missile2.move(1)
-            basic.pause(100)
-            missile1.turn(Direction.Right, 90)
-            missile2.move(-1)
-            basic.pause(100)
-            missile2.move(-1)
-            basic.pause(100)
-            missile2.delete()
-        }
-    }
+	
 })
